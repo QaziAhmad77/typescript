25941d4a2624e9c6be8609d12ce7ad3c454cac53
QaziAhmad77 <qaziahmad1877@gmail.com>
2024-05-04 00:59:54 +0500

    updated

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// let a: number = 25;
+let a = 25;
+// let b: string = "25";
+let b = "25";
+// let c: booloean = true;
+let c = true;
+// let d: any = "yesss"; 
+let d = "yesss";
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// let a: number = 25;
+let a = <number>25;
+// let b: string = "25";
+let b = <string>"25";
+// let c: booloean = true;
+let c = <boolean>true;
+// let d: any = "yesss"; 
+let d = <any>"yesss";
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
